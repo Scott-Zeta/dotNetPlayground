96dbb4adb65f3c1fb9412727a9a5bdb99f56289b
Scott Zeta <scott-zeta@outlook.com>
2024-03-15 15:05:23 +1030

    Path class

--- WorkingWithFile/Program.cs
+++ WorkingWithFile/Program.cs
@@ -8,6 +8,28 @@ foreach (var file in salesFiles)
   Console.WriteLine(file);
 }
 
+//get current running directory of the program
+Console.WriteLine(Directory.GetCurrentDirectory());
+
+//get the path of the user's documents folder, whatever the OS is
+string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+Console.WriteLine(docPath);
+
+// Path class can bu usefule to handle different slash in different OS
+// Always avoid hardcoding the path
+Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+Console.WriteLine(Path.Combine("stores", "201"));
+// Get extension
+Console.WriteLine(Path.GetExtension("sales.json"));
+
+// Get File info
+string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+
+FileInfo info = new FileInfo(fileName);
+
+Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
+
+
 IEnumerable<string> FindFiles(string folderName)
 {
   List<string> salesFiles = new List<string>();
