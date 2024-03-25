f1139bc72f2594b612357b88d76cdb4d0b4d1473
Scott Zeta <scott-zeta@outlook.com>
2024-03-25 17:16:52 +1030

    add the form binding to the newPizza instance, logic to POST request

--- ContosoPizza_Razor/Pages/PizzaList.cshtml.cs
+++ ContosoPizza_Razor/Pages/PizzaList.cshtml.cs
@@ -7,6 +7,21 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        [BindProperty] //This attribute is essential when dealing with POST requests
+        public Pizza NewPizza { get; set; } = default!;
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                // validation check by the class defined in Model file Pizza.cs
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList { get; set; } = default!;
 
