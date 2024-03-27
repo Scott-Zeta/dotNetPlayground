c87c3a9b13f5466a60840ecc9d968ca04dcb24ce
Scott Zeta <scott-zeta@outlook.com>
2024-03-27 16:52:07 +1030

    Add function

--- Fruit_APIRequest/FruitWebApp/Pages/Add.cshtml.cs
+++ Fruit_APIRequest/FruitWebApp/Pages/Add.cshtml.cs
@@ -13,15 +13,43 @@ namespace FruitWebApp.Pages
     {
         // IHttpClientFactory set using dependency injection 
         private readonly IHttpClientFactory _httpClientFactory;
-
-        public AddModel(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
+        private readonly ILogger<AddModel> _logger;
+        public AddModel(IHttpClientFactory httpClientFactory, ILogger<AddModel> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
 
         // Add the data model and bind the form data to the page model properties
         [BindProperty]
         public FruitModel FruitModels { get; set; }
 
+        /* By this section, browser user doesn't directly interact with the api endpoint
+        But the Page server send the POST request to the api endpoint */
+
         // Begin POST operation code
-        
+        public async Task<IActionResult> OnPost()
+        {
+            //parsing the content
+            var jsonContent = new StringContent(JsonSerializer.Serialize(FruitModels),
+                            Encoding.UTF8, "application/json");
+            _logger.LogInformation($"JSON content: {jsonContent.ReadAsStringAsync().Result}");
+
+            // Sending the POST request to the API
+            var httpClient = _httpClientFactory.CreateClient("FruitAPI");
+            using HttpResponseMessage response = await httpClient.PostAsync("", jsonContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["success"] = "Data was added successfully.";
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                TempData["failure"] = "Operation was not successful";
+                return RedirectToPage("Index");
+            }
+        }
         // End POST operation code
     }
 }
