efdbd625c4c77ca9eba4f4595e22e5717737b4ab
Scott Zeta <scott-zeta@outlook.com>
2024-05-05 19:49:07 +0930

    add EFcore route and method

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -5,6 +5,7 @@ using PizzaStore.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+// Add InMemory DB service to app
 builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
      {
@@ -21,6 +22,16 @@ if (app.Environment.IsDevelopment())
          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
      });
 }
+
+// Interact with DB by EF built-in functions
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
 
 app.Run();
 
