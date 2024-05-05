753852546e74d1a4c6f6c0f662fe782f4d4b84e8
Scott Zeta <scott-zeta@outlook.com>
2024-05-05 19:36:02 +0930

    add a tmp db to demo EF core

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.OpenApi.Models;
-using PizzaStore.DB;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
      {
          c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pizza Store API", Description = "Making the Pizzas you love", Version = "v1" });
@@ -19,12 +21,6 @@ if (app.Environment.IsDevelopment())
          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
      });
 }
-
-app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
-app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
-app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
 
